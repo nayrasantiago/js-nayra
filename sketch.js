ae94a29042c5c08b12a8c0d27a79c5d17267c7a0
naná <nayra.santiago@escola.pr.gov.br>
2024-08-15 17:16:01 -0300

    adicionando arquivo css e js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+    background( "white");
+  }
+  
+  function draw() {
+    
+    rect(30, 20, 20,);
+    fill ("blue");
+    stroke("black");
+    if( mouseIsPressed);{
+      rect (mouseX, mouseY, 20,20);}}
